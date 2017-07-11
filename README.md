2ffa371c962b5fd213982bf9f12aa75ac5dcd342
Rajat Pundir <rajatpundir13@gmail.com>
2017-07-11 22:04:44 +0530

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This is a relatively simple chess engine, with some quick and dirty functions to push pieces around the board.
 Someday, this chess engine might entertain the ideas of Deep Learning.
+
+There is further scope of improvement like: Castling, Stalemate, En Passant, and Checkmate.
+Further, pieces can be made wise in general, to not allow checkmate for their side, therefore all moves that allow your own side to be checkmated should discarded.
+But I leave these improvements in the hands of person who will need this chess engine to solve some chess problem.
--- chess.c
+++ chess.c
@@ -61,6 +61,13 @@ move *moves = NULL, *dangers = NULL;
 /*
 
   This is the documentation for the chess engine your are seeing right now, by its author Rajat Pundir.
+  It is a relatively simple chess engine, with some quick and dirty functions to push pieces around the board.
+  Someday, this chess engine might entertain the ideas of Deep Learning.
+
+  ***There is further scope of improvement like: Castling, Stalemate, En Passant, and Checkmate.***
+  ***Further, pieces can be made wise in general, to not allow checkmate for their side, therefore all moves that allow your own side to be checkmated should discarded.***
+  ***But I leave these improvements in the hands of person who will need this chess engine to solve some chess problem.***
+
 
   Here is a list of global variables and what they do:
   A. board
